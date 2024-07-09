224083b5845632e6a6cc5d2a83ee0307154be672
SergioSim <Sergio.Simonian@etu.unice.fr>
2024-07-09 11:30:27 +0200

    👷(project) add GitHub Actions CI workflow
    We set up a CI pipeline using GitHub Actions, adapted from
    Moodle Plugin CI, to ensure adherence to best development practices.

---
+++ gitlint/gitlint_emoji.py
@@ -0,0 +1,43 @@
+# Retrieved from https://github.com/openfun/ralph
+"""
+Gitlint extra rule to validate that the message title is of the form
+"<gitmoji>(<scope>) <subject>"
+"""
+
+from __future__ import unicode_literals
+
+import re
+
+import requests
+
+from gitlint.rules import CommitMessageTitle, LineRule, RuleViolation
+
+
+class GitmojiTitle(LineRule):
+    """Gitmoji title rule.
+
+    This rule will enforce that each commit title is of the form
+    "<gitmoji>(<scope>) <subject>" where gitmoji is an emoji from the list
+    defined in https://gitmoji.carloscuesta.me and subject should be all
+    lowercase.
+    """
+
+    id = "UC1"
+    name = "title-should-have-gitmoji-and-scope"
+    target = CommitMessageTitle
+
+    def validate(self, title, _commit):
+        """Validate Gitmoji title rule.
+
+        Download the list possible gitmojis from the project's GitHub
+        repository and check that title contains one of them.
+        """
+        gitmojis = requests.get(
+            "https://raw.githubusercontent.com/carloscuesta/gitmoji/master/packages/gitmojis/src/gitmojis.json",
+            timeout=5,
+        ).json()["gitmojis"]
+        emojis = [item["emoji"] for item in gitmojis]
+        pattern = r"^({:s})\(.*\)\s[a-z].*$".format("|".join(emojis))
+        if not re.search(pattern, title):
+            violation_msg = 'Title does not match regex "<gitmoji>(<scope>) <subject>"'
+            return [RuleViolation(self.id, violation_msg, title)]
